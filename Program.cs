b8553a66c436c09c89ecf7e0b6c5d56e092e129b
CunningPie <Makarikhin.P.A@gmail.com>
2021-09-10 14:26:36 +0300

    Added functionality

--- Program.cs
+++ Program.cs
@@ -8,9 +8,31 @@ namespace PrimeNumbers
 {
     class Program
     {
+        static List<int> PrimeNumbers = new List<int>();
+
+        static bool IsPrimeNumber(int N)
+        {
+            for (int i = 2; i < N; i++)
+                if (N % i == 0)
+                    return false;
+            return true;
+        }
+
+        static void AddPrimeNumber(int N)
+        {
+            if (IsPrimeNumber(N))
+                PrimeNumbers.Add(N);
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Input number: ");
+            Double.TryParse(Console.ReadLine(), out double N);
 
+            Parallel.For(2, (int)(N + 1), AddPrimeNumber);
+            foreach (int i in PrimeNumbers)
+                Console.Write(i + " ");
+            Console.ReadKey();
         }
     }
 }
